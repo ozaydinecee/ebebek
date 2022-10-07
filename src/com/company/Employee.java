08ee1102e1dbf774c407b117821141c20162f186
Ece Özaydın <ozaydinecee@gmail.com>
2022-10-07 21:16:43 +0300

    case completed

---
+++ src/com/company/Employee.java
@@ -0,0 +1,69 @@
+package com.company;
+
+public class Employee {
+    String name;
+    int salary;
+    int workHours;
+    int hireYear;
+
+    // Constructor would initialize data members
+    // With the values of passed arguments while
+    // Object of that class created
+    Employee(String name, int salary,int workHours,int hireYear)
+    {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+
+    }
+    public double tax(int salary) {
+        double tax = 0;
+        if (salary < 1000) {
+            System.out.println("No tax needed.");
+        } else {
+            tax = salary *(0.03);
+        }
+        return tax;
+    }
+
+    public int bonus(int workHours){
+        int bonus=0;
+        if (workHours>40){
+            bonus=(workHours-40)*30;
+        }
+        else{
+            System.out.println("Employee doesn't have bonus.");
+
+        }
+        return bonus;
+    }
+
+    public double raiseSalary(int hireYear, int salary, int workHours){
+        double currentSalary=salary-tax(salary)+bonus(workHours);
+        int currentYear=2021;
+        int workingYear=currentYear - hireYear;
+        double raisingAmount=0;
+
+        if(workingYear<10){
+            raisingAmount=currentSalary*(0.05);
+        }
+        else if(workingYear>9 && workingYear<20){
+
+            raisingAmount=currentSalary*(0.10);
+        }
+        else{
+            raisingAmount=currentSalary*(0.15);
+        }
+        return raisingAmount;
+    }
+
+    public String toString(){//overriding the toString() method
+        return "Employee full-name is "+name+ ". It's monthly salary is "+salary+ ". It's hired in "+hireYear+
+                ". It works "+workHours+ " hours in a week."+ " Thus bonus amount is "+bonus(workHours)+
+                ". Tax amount is "+tax(salary)+". Raising amount of the salary is "+raiseSalary(hireYear,salary,workHours)+"."
+                ;
+    }
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Employee employee1= new Employee("ece özaydın", 2000,43,2020);
+        int salary = employee1.salary;
+        System.out.println(employee1.toString());
+
+    }
+}
